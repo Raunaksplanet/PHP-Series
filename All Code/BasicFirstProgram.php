c0441ed78ac92ead74b0776ab81822ab0d6dd8af
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-09-24 20:43:44 +0530

    Update BasicFirstProgram.php

--- All Code/BasicFirstProgram.php
+++ All Code/BasicFirstProgram.php
@@ -1,15 +1,17 @@
 <?php
 
-// 1.echo keyword is use to get stdout (standard output)
-   echo "Hello World"; 
+// 1.echo keyword is use to get stdout (standard output).
+   echo "Hello World", " to ", "Ruank Gupta" ; 
 
-// 2.this is single line comment
+// 2.this is single line comment.
 /*  
    3.this is
    multiLine Comment
 */
 
-// 4.br tag is use to get new line in PHP
+// 4.br tag is use to get new line in PHP.
    echo "<br>";
    echo "How are you ?";
+
+// 5.You can use !(esclanation mark) to get basic template of html.
 ?>
