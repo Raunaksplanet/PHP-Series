8d50ee870f45017cca1637a6b210f4bce9e213c4
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-09-24 19:55:39 +0530

    Update BasicFirstProgram.php

--- All Code/BasicFirstProgram.php
+++ All Code/BasicFirstProgram.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello World"; //Echo keyword is use to get stdout (standard output)
+echo "Hello World"; //echo keyword is use to get stdout (standard output)
 
 // this is single line comment
 /*  
