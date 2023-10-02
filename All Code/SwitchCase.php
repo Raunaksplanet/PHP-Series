7c7ced8563f7e56b00608064b34dcf2b9bba4b36
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-10-02 10:04:43 +0530

    Create SwitchCase.php

---
+++ All Code/SwitchCase.php
@@ -0,0 +1,22 @@
+<?php
+/*
+    Control Flow Statement
+    1.3 Switch Statement
+*/
+
+    $var1 = 6;
+    switch($var1){
+        case 1: echo "1";
+        break;
+        case 2: echo "2";
+        break;
+        case 3: echo "3";
+        break;
+        case 4: echo "4";
+        break;
+        case 5: echo "5";
+        break;
+        default:
+        echo "Default";
+    }
+?>
