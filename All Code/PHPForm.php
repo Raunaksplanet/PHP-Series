b69e162e4f01d0a1e37ab24dc09bf99d61c21e02
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-10-04 21:41:59 +0530

    Create PHPForm.php

---
+++ All Code/PHPForm.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPForm</title>
+    
+</head>
+<body>
+    <div>
+        <form action="PHPForm.php" method="post">
+            <label for="Name">Name:</label><br>
+            <input type="text" name="name"><br><br>
+            <label for="Password">Password:</label><br>
+            <input type="password" name="pass"><br>
+            <input type="submit" value="login">
+        </form>
+    </div>
+    <br><br>
+    <?php
+    $name = $_POST["name"];
+    $pass = $_POST["pass"];
+
+    echo "Your name is " . $name ."<br>";
+    echo "Your password is " . $pass ."<br>";
+    ?>
+</body>
+</html>
