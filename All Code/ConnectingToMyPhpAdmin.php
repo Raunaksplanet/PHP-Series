718a3734f1c2ae928748c8da460d0e3e4d5ac0e2
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-10-05 21:58:43 +0530

    Create ConnectingToMyPhpAdmin.php

---
+++ All Code/ConnectingToMyPhpAdmin.php
@@ -0,0 +1,14 @@
+<?php  
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    $conn = mysqli_connect($servername,$username,$password);
+
+    if(!$conn){
+        die("Connection Failed: " . mysqli_connect_error());
+    }
+    else{
+        echo "Connection Successful";
+    }
+?>
