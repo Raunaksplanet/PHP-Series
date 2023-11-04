33d4d58c9f05c068d9fef698a9dbd18e7deec38e
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-11-04 08:49:57 +0530

    Add files via upload

---
+++ PHP Assignment/assignment.php
@@ -0,0 +1,23 @@
+<?php
+
+// 1.echo keyword is use to get stdout (standard output).
+   echo "Hello World", " to ", "Ruank Gupta" ; 
+
+// 2.this is single line comment.
+/*  
+   3.this is
+   multiLine Comment
+*/
+
+// 4.br tag is use to get new line in PHP.
+   echo "<br>";
+   echo "How are you ?";
+
+// 5.You can use !(esclanation mark) to get basic template of html.
+
+// 6. Constants.
+   define("var1",100);
+   const var2 = "sadf";
+
+   echo var1 . "<br>" . var2;
+?>
